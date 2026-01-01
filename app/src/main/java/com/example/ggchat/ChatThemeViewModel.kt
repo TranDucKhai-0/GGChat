1e020dd5dfec471337607f7eee0828fbb8687013
Tran Duc Khai <khaixayda@gmail.com>
2026-01-01 15:08:27 +0700

    Chỉnh cỡ chữ

--- app/src/main/java/com/example/ggchat/ChatThemeViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ggchat
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class ChatThemeViewModel : ViewModel() {
-    val isDarkMode = MutableLiveData(false)
-}
